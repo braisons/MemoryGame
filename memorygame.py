29b63735b1ee244b3f7c65a6d15537ee9bd97670
Braison Sterling <bsterlng912@gmail.com>
2024-02-12 14:40:22 -0500

    Initial Commit

---
+++ memorygame.py
@@ -0,0 +1,51 @@
+# Author
+# Date
+# Desription
+# Revision History
+# Name       Date     Desc
+#Braison    2/12/24   Initial code base
+#
+#
+
+
+import random
+import turtle
+import time
+
+number_list = []
+start_pos_x = -100
+start_pos_y = 0
+level = 3
+offset = 100
+
+def drawSquare(x, y):
+
+    turtle.down()
+    for x in range(4):
+        turtle.forward(100)
+        turtle.left(90)
+
+
+for x in range(level):
+    # data type is integer
+    a = random.randint(1,99)
+
+
+   # data structure
+    number_list.append(a)
+    
+    turtle.write(str(a), font=("Verdana",50, "normal"))
+    turtle.up()
+    save_pos = turtle.goto(x * offset,0)
+    turtle.goto(save_pos)
+
+    drawSquare(x * offset, 0)
+    turtle.goto(save_pos)
+    time.sleep(3)
+     
+turtle.clear()
+
+# please enter the answers
+for x in range(level):
+    answer = int(input("The numbers were: "))
+
